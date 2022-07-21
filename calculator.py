940a910867934277c0fa751d74edc165f0dcba2a
SOHAM MAITY <89208925+SpreadSheets650@users.noreply.github.com>
2022-07-21 20:04:45 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,75 @@
+
+# Author : Soham Maity
+# Class : IX Science A
+
+# Program make a simple calculator
+
+# operation functions
+
+# This function adds two numbers
+def add(x, y):
+    return x + y
+
+# This function subtracts two numbers
+def subtract(x, y):
+    return x - y
+
+# This function multiplies two numbers
+def multiply(x, y):
+    return x * y
+
+# This function divides two numbers
+def divide(x, y):
+    return x / y
+
+# UI - printing statements
+
+print("Select An Operation To Perform :")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+# logic functions
+
+while True:
+    # take input from the user
+    choice = input("Enter Your Choice In ( 1 / 2 / 3 / 4 ) : ")
+
+    # check if choice is one of the four options
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+        
+        # check if user wants another calculation
+        # break the while loop if answer is no
+        next_calculation = input("Should We Do Another Calculation ? ( Yes / No ) : ")
+        if next_calculation == "no":
+            print ("Thank You For Using My Calculator | Please Rate It Bettween 0 - 10")
+            rate = int(input("Your Rating : "))
+            
+        if rate <= 5:
+            print ("It's Sad To Hear That, I Will Try To Improve Next Time")
+        if rate > 5:
+            print ("Thank You For Your Amazing Rate")
+        if rate >= 10:
+            print ("WoW Your Rating Is Beyond Imagination | Thank You")
+        
+        
+            break
+     
+    
+    else:
+        print("Invalid Input")
